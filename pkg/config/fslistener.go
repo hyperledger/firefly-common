440fcceceaae29a8d16d4a662acb7ab841962976
Peter Broadhurst <peter.broadhurst@kaleido.io>
2022-12-09 14:32:17 -0500

    Address limitations in viper config listener
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ pkg/config/fslistener.go
@@ -0,0 +1,93 @@
+// Copyright © 2022 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"context"
+	"os"
+	"path"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/hyperledger/firefly-common/pkg/i18n"
+	"github.com/hyperledger/firefly-common/pkg/log"
+	"github.com/spf13/viper"
+)
+
+// Listens for changes to the configuration file configured in Viper.
+// Note this:
+// - Only ends if the context is closed
+// - Listens at the directory level, for cross OS compatibility
+// - Only fires if the config file has changed, and exists ok
+// - Tries to only fire once if possible
+// - Does not reload the config - that's the caller's responsibility
+func WatchConfig(ctx context.Context, onChange, onClose func()) error {
+
+	// Note that while viper.WatchConfig() exists, it doesn't handle various
+	// types of config change. Specifically it doesn't handle the case where the
+	// removed event comes from the filesystem, before the create/update event(s).
+	// The listener just ends in that case, and stops notifying of events :shrug
+
+	fullConfigFilePath := viper.ConfigFileUsed()
+	filePath := path.Dir(fullConfigFilePath)
+	fileName := path.Base(fullConfigFilePath)
+	log.L(ctx).Debugf("Starting config file listener for '%s' in directory '%s'", fileName, filePath)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err == nil {
+		go fsListenerLoop(ctx, fileName, onChange, func() {
+			_ = watcher.Close()
+			if onClose != nil {
+				onClose()
+			}
+		}, watcher.Events, watcher.Errors)
+		err = watcher.Add(filePath)
+	}
+	if err != nil {
+		log.L(ctx).Errorf("Failed to start filesystem listener: %s", err)
+		return i18n.WrapError(ctx, err, i18n.MsgFailedToStartListener, err)
+	}
+	return nil
+}
+
+func fsListenerLoop(ctx context.Context, fileName string, onChange, onClose func(), events chan fsnotify.Event, errors chan error) {
+	defer onClose()
+
+	for {
+		select {
+		case <-ctx.Done():
+			// The only case where we end, is when the context is closed
+			log.L(ctx).Infof("File listener exiting")
+			return
+		case event := <-events:
+			isUpdate := (event.Op&(fsnotify.Create|fsnotify.Rename|fsnotify.Write) != 0)
+			log.L(ctx).Debugf("FSEvent [%s] update=%t: %s", event.Op, isUpdate, event.Name)
+			if path.Base(event.Name) == fileName {
+				if event.Op&(fsnotify.Create|fsnotify.Rename|fsnotify.Write) != 0 {
+					fi, err := os.Stat(event.Name)
+					if err == nil && !fi.IsDir() {
+						log.L(ctx).Infof("Config file change detected. Event=%s Name=%s Size=%d", event.Op, event.Name, fi.Size())
+						onChange()
+					}
+				}
+			}
+		case err, ok := <-errors:
+			if ok {
+				log.L(ctx).Errorf("FSEvent error: %s", err)
+			}
+		}
+	}
+}
---
+++ pkg/config/fslistener_test.go
@@ -0,0 +1,108 @@
+// Copyright © 2022 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFileListenerE2E(t *testing.T) {
+
+	logrus.SetLevel(logrus.DebugLevel)
+	tmpDir := t.TempDir()
+
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(fmt.Sprintf("%s/test.yaml", tmpDir))
+
+	// Start listener on empty dir
+	fsListenerDone := make(chan struct{})
+	fsListenerFired := make(chan bool)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	err := WatchConfig(ctx, func() {
+		err := viper.ReadInConfig()
+		assert.NoError(t, err)
+		fsListenerFired <- true
+	}, func() {
+		close(fsListenerDone)
+	})
+	assert.NoError(t, err)
+
+	// Create the file
+	os.WriteFile(fmt.Sprintf("%s/test.yaml", tmpDir), []byte(`{"ut_conf": "one"}`), 0664)
+	<-fsListenerFired
+	assert.Equal(t, "one", viper.Get("ut_conf"))
+
+	// Write an update
+	os.WriteFile(fmt.Sprintf("%s/test.yaml", tmpDir), []byte(`{"ut_conf": "two"}`), 0664)
+	<-fsListenerFired
+	assert.Equal(t, "two", viper.Get("ut_conf"))
+
+	// Rename in another file
+	os.WriteFile(fmt.Sprintf("%s/another.yaml", tmpDir), []byte(`{"ut_conf": "three"}`), 0664)
+	os.Rename(fmt.Sprintf("%s/another.yaml", tmpDir), fmt.Sprintf("%s/test.yaml", tmpDir))
+	<-fsListenerFired
+	assert.Equal(t, "three", viper.Get("ut_conf"))
+
+	// Delete and recreate
+	os.Remove(fmt.Sprintf("%s/test.yaml", tmpDir))
+	os.WriteFile(fmt.Sprintf("%s/test.yaml", tmpDir), []byte(`{"ut_conf": "four"}`), 0664)
+	<-fsListenerFired
+	assert.Equal(t, "four", viper.Get("ut_conf"))
+
+	defer func() {
+		cancelCtx()
+		if a := recover(); a != nil {
+			panic(a)
+		}
+		<-fsListenerDone
+	}()
+
+}
+
+func TestFileListenerFail(t *testing.T) {
+
+	logrus.SetLevel(logrus.DebugLevel)
+	tmpDir := t.TempDir()
+	os.RemoveAll(tmpDir)
+
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(fmt.Sprintf("%s/test.yaml", tmpDir))
+
+	err := WatchConfig(context.Background(), nil, nil)
+	assert.Regexp(t, "FF00194", err)
+}
+
+func TestFileListenerLogError(t *testing.T) {
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	defer cancelCtx()
+	errors := make(chan error)
+	fsListenerDone := make(chan struct{})
+	go fsListenerLoop(ctx, "somefile", func() {}, func() { close(fsListenerDone) }, make(chan fsnotify.Event), errors)
+
+	errors <- fmt.Errorf("pop")
+	cancelCtx()
+	<-fsListenerDone
+}
