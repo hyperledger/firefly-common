f90294ab7e1eea7f0749ec2a438273a9d61f20de
Peter Broadhurst <peter.broadhurst@kaleido.io>
2023-03-29 09:18:56 +0100

    Add cobra command for printing config
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ pkg/config/cobracmd.go
@@ -0,0 +1,40 @@
+// Copyright © 2023 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func ShowConfigCobraCommand(initConf func()) *cobra.Command {
+	return &cobra.Command{
+		Use:     "showconfig",
+		Aliases: []string{"showconf"},
+		Short:   "List out the configuration options",
+		Run: func(cmd *cobra.Command, args []string) {
+			initConf()
+
+			fmt.Printf("%-64s %v\n", "Key", "Value")
+			fmt.Print("-----------------------------------------------------------------------------------\n")
+			for _, k := range GetKnownKeys() {
+				fmt.Printf("%-64s %v\n", k, Get(RootKey(k)))
+			}
+		},
+	}
+}
