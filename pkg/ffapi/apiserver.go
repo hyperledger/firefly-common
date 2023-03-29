6be11ccff2549ed02501226d6b2b93cdc3c19917
Peter Broadhurst <peter.broadhurst@kaleido.io>
2023-03-29 13:40:32 +0100

    Add a fully functional APIServer into common
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

---
+++ pkg/ffapi/apiserver.go
@@ -0,0 +1,274 @@
+// Copyright © 2023 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ffapi
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/ghodss/yaml"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/gorilla/mux"
+	"github.com/hyperledger/firefly-common/pkg/config"
+	"github.com/hyperledger/firefly-common/pkg/httpserver"
+	"github.com/hyperledger/firefly-common/pkg/i18n"
+	"github.com/hyperledger/firefly-common/pkg/metric"
+)
+
+const APIServerMetricsSubSystemName = "api_server_rest"
+
+// APIServer is an opinionated use of the HTTP Server facilities in common, to provide
+// an API server with:
+// - a set of routes defining API endpoints
+// - a swagger endpoint on /api
+// - optional metrics endpoint on /metrics (configurable)
+type APIServer interface {
+	Serve(ctx context.Context) error
+	Started() <-chan struct{}
+	APIPublicURL() string // valid to call after server is successfully started
+}
+
+type apiServer[T any] struct {
+	started chan struct{}
+
+	defaultFilterLimit uint64
+	maxFilterLimit     uint64
+	maxFilterSkip      uint64
+	requestTimeout     time.Duration
+	requestMaxTimeout  time.Duration
+	apiPublicURL       string
+	metricsEnabled     bool
+	metricsPath        string
+	metricsPublicURL   string
+
+	APIServerOptions[T]
+}
+
+type APIServerOptions[T any] struct {
+	MetricsRegistry  metric.MetricsRegistry
+	Routes           []*Route
+	GetContextObject func(r *APIRequest) T
+	Description      string
+	APIConfig        config.Section
+	MetricsConfig    config.Section
+	CORSConfig       config.Section
+}
+
+type APIServerRouteExt[T any] struct {
+	JSONHandler   func(*APIRequest, T) (output interface{}, err error)
+	UploadHandler func(*APIRequest, T) (output interface{}, err error)
+}
+
+// NewAPIServer makes a new server, with the specified configuration, and
+// the supplied wrapper function - which will inject
+func NewAPIServer[T any](ctx context.Context, options APIServerOptions[T]) APIServer {
+	as := &apiServer[T]{
+		defaultFilterLimit: options.APIConfig.GetUint64(ConfAPIDefaultFilterLimit),
+		maxFilterLimit:     options.APIConfig.GetUint64(ConfAPIMaxFilterLimit),
+		maxFilterSkip:      options.APIConfig.GetUint64(ConfAPIMaxFilterSkip),
+		requestTimeout:     options.APIConfig.GetDuration(ConfAPIRequestTimeout),
+		requestMaxTimeout:  options.APIConfig.GetDuration(ConfAPIRequestMaxTimeout),
+		metricsEnabled:     options.MetricsConfig.GetBool(ConfMetricsServerEnabled),
+		metricsPath:        options.MetricsConfig.GetString(ConfMetricsServerPath),
+		APIServerOptions:   options,
+		started:            make(chan struct{}),
+	}
+	_ = as.MetricsRegistry.NewHTTPMetricsInstrumentationsForSubsystem(
+		ctx,
+		APIServerMetricsSubSystemName,
+		true,
+		prometheus.DefBuckets,
+		map[string]string{},
+	)
+	return as
+}
+
+// Serve is the main entry point for the API Server
+func (as *apiServer[T]) Serve(ctx context.Context) (err error) {
+	started := false
+	defer func() {
+		// Ensure we don't leave the Started() channel indefinitely in the case of an error
+		if !started {
+			close(as.started)
+		}
+	}()
+
+	httpErrChan := make(chan error)
+	metricsErrChan := make(chan error)
+
+	apiHTTPServer, err := httpserver.NewHTTPServer(ctx, "api", as.createMuxRouter(ctx, as.apiPublicURL), httpErrChan, as.APIConfig, as.CORSConfig, &httpserver.ServerOptions{
+		MaximumRequestTimeout: as.requestMaxTimeout,
+	})
+	if err != nil {
+		return err
+	}
+	as.apiPublicURL = buildPublicURL(as.APIConfig, apiHTTPServer.Addr())
+	go apiHTTPServer.ServeHTTP(ctx)
+
+	if as.metricsEnabled {
+		metricsHTTPServer, err := httpserver.NewHTTPServer(ctx, "metrics", as.createMetricsMuxRouter(ctx), metricsErrChan, as.MetricsConfig, as.CORSConfig, &httpserver.ServerOptions{
+			MaximumRequestTimeout: as.requestMaxTimeout,
+		})
+		if err != nil {
+			return err
+		}
+		as.metricsPublicURL = buildPublicURL(as.MetricsConfig, apiHTTPServer.Addr())
+		go metricsHTTPServer.ServeHTTP(ctx)
+	}
+
+	started = true
+	close(as.started)
+	return as.waitForServerStop(httpErrChan, metricsErrChan)
+}
+
+func (as *apiServer[T]) Started() <-chan struct{} {
+	return as.started
+}
+
+func (as *apiServer[T]) APIPublicURL() string {
+	return as.apiPublicURL
+}
+
+func (as *apiServer[T]) waitForServerStop(httpErrChan, metricsErrChan chan error) error {
+	select {
+	case err := <-httpErrChan:
+		return err
+	case err := <-metricsErrChan:
+		return err
+	}
+}
+
+func buildPublicURL(conf config.Section, a net.Addr) string {
+	publicURL := conf.GetString(httpserver.HTTPConfPublicURL)
+	if publicURL == "" {
+		proto := "https"
+		if !conf.GetBool(httpserver.HTTPConfTLSEnabled) {
+			proto = "http"
+		}
+		publicURL = fmt.Sprintf("%s://%s", proto, a.String())
+	}
+	return publicURL
+}
+
+func (as *apiServer[T]) swaggerGenConf(apiBaseURL string) *Options {
+	return &Options{
+		BaseURL:                   apiBaseURL,
+		Title:                     as.Description,
+		Version:                   "1.0",
+		PanicOnMissingDescription: false,
+		DefaultRequestTimeout:     as.requestTimeout,
+	}
+}
+
+func (as *apiServer[T]) swaggerHandler(generator func(req *http.Request) (*openapi3.T, error)) func(res http.ResponseWriter, req *http.Request) (status int, err error) {
+	return func(res http.ResponseWriter, req *http.Request) (status int, err error) {
+		vars := mux.Vars(req)
+		doc, err := generator(req)
+		if err != nil {
+			return 500, err
+		}
+		if vars["ext"] == ".json" {
+			res.Header().Add("Content-Type", "application/json")
+			b, _ := json.Marshal(&doc)
+			_, _ = res.Write(b)
+		} else {
+			res.Header().Add("Content-Type", "application/x-yaml")
+			b, _ := yaml.Marshal(&doc)
+			_, _ = res.Write(b)
+		}
+		return 200, nil
+	}
+}
+
+func (as *apiServer[T]) swaggerGenerator(apiBaseURL string) func(req *http.Request) (*openapi3.T, error) {
+	swg := NewSwaggerGen(as.swaggerGenConf(apiBaseURL))
+	return func(req *http.Request) (*openapi3.T, error) {
+		return swg.Generate(req.Context(), as.Routes), nil
+	}
+}
+func (as *apiServer[T]) routeHandler(hf *HandlerFactory, apiBaseURL string, route *Route) http.HandlerFunc {
+	// We extend the base ffapi functionality, with standardized DB filter support for all core resources.
+	// We also pass the Orchestrator context through
+	ext := route.Extensions.(*APIServerRouteExt[T])
+	route.JSONHandler = func(r *APIRequest) (output interface{}, err error) {
+		return ext.JSONHandler(r, as.GetContextObject(r))
+	}
+	return hf.RouteHandler(route)
+}
+
+func (as *apiServer[T]) handlerFactory() *HandlerFactory {
+	return &HandlerFactory{
+		DefaultRequestTimeout: as.requestTimeout,
+		MaxTimeout:            as.requestMaxTimeout,
+	}
+}
+
+func (as *apiServer[T]) createMuxRouter(ctx context.Context, publicURL string) *mux.Router {
+	r := mux.NewRouter().UseEncodedPath()
+	hf := as.handlerFactory()
+
+	if as.metricsEnabled {
+		h, _ := as.MetricsRegistry.GetHTTPMetricsInstrumentationsMiddlewareForSubsystem(ctx, APIServerMetricsSubSystemName)
+		r.Use(h)
+	}
+
+	apiBaseURL := fmt.Sprintf("%s/api/v1", publicURL)
+	for _, route := range as.Routes {
+		ce, ok := route.Extensions.(*APIServerRouteExt[T])
+		if !ok {
+			panic(fmt.Sprintf("invalid route extensions: %t", route.Extensions))
+		}
+		if ce.UploadHandler != nil {
+			route.FormUploadHandler = func(r *APIRequest) (output interface{}, err error) {
+				return ce.UploadHandler(r, as.GetContextObject(r))
+			}
+		}
+		if ce.JSONHandler != nil || ce.UploadHandler != nil {
+			r.HandleFunc(fmt.Sprintf("/api/v1/%s", route.Path), as.routeHandler(hf, apiBaseURL, route)).
+				Methods(route.Method)
+		}
+	}
+
+	r.HandleFunc(`/api/swagger{ext:\.yaml|\.json|}`, hf.APIWrapper(as.swaggerHandler(as.swaggerGenerator(apiBaseURL))))
+	r.HandleFunc(`/api`, hf.APIWrapper(hf.SwaggerUIHandler(publicURL+"/api/swagger.yaml")))
+	r.HandleFunc(`/favicon{any:.*}.png`, favIcons)
+
+	r.NotFoundHandler = hf.APIWrapper(as.notFoundHandler)
+	return r
+}
+
+func (as *apiServer[T]) notFoundHandler(res http.ResponseWriter, req *http.Request) (status int, err error) {
+	res.Header().Add("Content-Type", "application/json")
+	return 404, i18n.NewError(req.Context(), i18n.Msg404NotFound)
+}
+
+func (as *apiServer[T]) createMetricsMuxRouter(ctx context.Context) *mux.Router {
+	r := mux.NewRouter()
+	h, err := as.MetricsRegistry.HTTPHandler(ctx, promhttp.HandlerOpts{})
+	if err != nil {
+		panic(err)
+	}
+	r.Path(as.metricsPath).Handler(h)
+	return r
+}
---
+++ pkg/ffapi/apiserver_test.go
@@ -0,0 +1,331 @@
+// Copyright © 2023 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ffapi
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/go-resty/resty/v2"
+	"github.com/hyperledger/firefly-common/pkg/config"
+	"github.com/hyperledger/firefly-common/pkg/httpserver"
+	"github.com/hyperledger/firefly-common/pkg/metric"
+	"github.com/stretchr/testify/assert"
+)
+
+// utManager simulate the type you'd pass through your request interface
+type utManager struct {
+	t                   *testing.T
+	mockErr             error
+	calledJSONHandler   string
+	calledUploadHandler string
+}
+
+type sampleInput struct {
+	Input1 string `json:"input1"`
+}
+
+type sampleOutput struct {
+	Output1 string `json:"output1"`
+}
+
+// utAPIRoute1 is a simple demonstration route
+var utAPIRoute1 = &Route{
+	Name:        "utAPIRoute1",
+	Path:        "ut/utresource/{resourceid}/postit",
+	Method:      http.MethodPost,
+	Description: "simulated rather random POST route good for testing", /* should be translated in real app */
+	PathParams: []*PathParam{
+		{Name: "resourceid", Description: "My resource" /* should be translated in real app */},
+	},
+	FormParams: []*FormParam{
+		{Name: "test_field"},
+	},
+	JSONInputValue:  func() interface{} { return &sampleInput{} },
+	JSONOutputValue: func() interface{} { return &sampleOutput{} },
+	JSONOutputCodes: []int{http.StatusOK},
+	Extensions: &APIServerRouteExt[*utManager]{
+		JSONHandler: func(r *APIRequest, um *utManager) (output interface{}, err error) {
+			um.calledJSONHandler = r.PP["resourceid"]
+			assert.Equal(um.t, "test_json_input", r.Input.(*sampleInput).Input1)
+			return &sampleOutput{Output1: "test_json_output"}, um.mockErr
+		},
+		UploadHandler: func(r *APIRequest, um *utManager) (output interface{}, err error) {
+			um.calledJSONHandler = r.PP["resourceid"]
+			assert.Equal(um.t, "test_form_input", r.FP["test_field"])
+			d, err := io.ReadAll(r.Part.Data)
+			assert.NoError(um.t, err)
+			assert.Equal(um.t, "test_form_data", string(d))
+			return &sampleOutput{Output1: "test_form_output"}, um.mockErr
+		},
+	},
+}
+
+func initUTConfig() (config.Section, config.Section, config.Section) {
+	config.RootConfigReset()
+	apiConfig := config.RootSection("ut.api")
+	metricsConfig := config.RootSection("ut.metrics")
+	corsConfig := config.RootSection("ut.cors")
+	InitAPIServerConfig(apiConfig, metricsConfig, corsConfig)
+	apiConfig.Set(httpserver.HTTPConfPort, 0)
+	metricsConfig.Set(httpserver.HTTPConfPort, 0)
+	return apiConfig, metricsConfig, corsConfig
+}
+
+func newTestAPIServer(t *testing.T, start bool) (*utManager, *apiServer[*utManager], func()) {
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	apiConfig, metricsConfig, corsConfig := initUTConfig()
+	um := &utManager{t: t}
+	as := NewAPIServer(ctx, APIServerOptions[*utManager]{
+		MetricsRegistry: metric.NewPrometheusMetricsRegistry("ut"),
+		Routes:          []*Route{utAPIRoute1},
+		GetContextObject: func(r *APIRequest) *utManager {
+			// This could be some dynamic object based on extra processing in the request,
+			// but the most common case is you just have a "manager" that you inject into each
+			// request and that's the "T" on the APIServer
+			return um
+		},
+		Description:   "unit testing",
+		APIConfig:     apiConfig,
+		MetricsConfig: metricsConfig,
+		CORSConfig:    corsConfig,
+	})
+	done := make(chan struct{})
+	if start {
+		go func() {
+			err := as.Serve(ctx)
+			assert.NoError(t, err)
+			close(done)
+		}()
+	} else {
+		close(done)
+	}
+	return um, as.(*apiServer[*utManager]), func() {
+		cancelCtx()
+		<-done
+	}
+}
+
+func TestAPIServerInvokeAPIRouteJSON(t *testing.T) {
+	um, as, done := newTestAPIServer(t, true)
+	defer done()
+
+	<-as.Started()
+
+	var o sampleOutput
+	res, err := resty.New().R().
+		SetBody(&sampleInput{
+			Input1: "test_json_input",
+		}).
+		SetResult(&o).
+		Post(fmt.Sprintf("%s/api/v1/ut/utresource/id12345/postit", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode())
+	assert.Equal(t, "id12345", um.calledJSONHandler)
+	assert.Equal(t, "test_json_output", o.Output1)
+}
+
+func TestAPIServerInvokeAPIRouteForm(t *testing.T) {
+	um, as, done := newTestAPIServer(t, true)
+	defer done()
+
+	<-as.Started()
+
+	var o sampleOutput
+	res, err := resty.New().R().
+		SetMultipartFormData(map[string]string{
+			"test_field": "test_form_input",
+		}).
+		SetMultipartField("data", "file1", "text.plain", strings.NewReader("test_form_data")).
+		SetResult(&o).
+		Post(fmt.Sprintf("%s/api/v1/ut/utresource/id12345/postit", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode())
+	assert.Equal(t, "id12345", um.calledJSONHandler)
+	assert.Equal(t, "test_form_output", o.Output1)
+
+}
+
+func TestAPIServerInvokeAPIRouteCheckPathEscaping(t *testing.T) {
+	um, as, done := newTestAPIServer(t, true)
+	defer done()
+
+	<-as.Started()
+
+	var o sampleOutput
+	res, err := resty.New().R().
+		SetBody(&sampleInput{
+			Input1: "test_json_input",
+		}).
+		SetResult(&o).
+		Post(fmt.Sprintf("%s/api/v1/ut/utresource/foo%%2Fbar/postit", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode())
+	assert.Equal(t, "foo/bar", um.calledJSONHandler)
+	assert.Equal(t, "test_json_output", o.Output1)
+}
+
+func TestAPIServerSwaggerJSON(t *testing.T) {
+	_, as, done := newTestAPIServer(t, true)
+	defer done()
+
+	<-as.Started()
+
+	res, err := resty.New().R().
+		SetDoNotParseResponse(true).
+		Put(fmt.Sprintf("%s/api/swagger.json", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode())
+	assert.Regexp(t, "application/json", res.Header().Get("content-type"))
+}
+
+func TestAPIServerSwaggerYAML(t *testing.T) {
+	_, as, done := newTestAPIServer(t, true)
+	defer done()
+
+	<-as.Started()
+
+	res, err := resty.New().R().
+		SetDoNotParseResponse(true).
+		Put(fmt.Sprintf("%s/api/swagger.yaml", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode())
+	assert.Regexp(t, "application/x-yaml", res.Header().Get("content-type"))
+}
+
+func TestAPIServerSwaggerUI(t *testing.T) {
+	_, as, done := newTestAPIServer(t, true)
+	defer done()
+
+	<-as.Started()
+
+	res, err := resty.New().R().
+		SetDoNotParseResponse(true).
+		Put(fmt.Sprintf("%s/api", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode())
+	assert.Regexp(t, "text/html", res.Header().Get("content-type"))
+}
+
+func TestAPIServer404(t *testing.T) {
+	_, as, done := newTestAPIServer(t, true)
+	defer done()
+
+	<-as.Started()
+
+	res, err := resty.New().R().
+		SetDoNotParseResponse(true).
+		Put(fmt.Sprintf("%s/wrong", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 404, res.StatusCode())
+}
+
+func TestAPIServer500(t *testing.T) {
+	um, as, done := newTestAPIServer(t, true)
+	defer done()
+	um.mockErr = fmt.Errorf("pop")
+
+	<-as.Started()
+
+	res, err := resty.New().R().
+		SetBody(&sampleInput{
+			Input1: "test_json_input",
+		}).
+		Post(fmt.Sprintf("%s/api/v1/ut/utresource/id12345/postit", as.APIPublicURL()))
+	assert.NoError(t, err)
+	assert.Equal(t, 500, res.StatusCode())
+}
+
+func TestAPIServerFailServe(t *testing.T) {
+	_, as, done := newTestAPIServer(t, false)
+	defer done()
+
+	as.APIConfig.Set(httpserver.HTTPConfAddress, "!badness")
+	err := as.Serve(context.Background())
+	assert.Regexp(t, "FF00151", err)
+
+	// Check we still closed the started channel
+	<-as.Started()
+
+}
+
+func TestAPIServerFailServeMetrics(t *testing.T) {
+	_, as, done := newTestAPIServer(t, false)
+	defer done()
+
+	as.MetricsConfig.Set(httpserver.HTTPConfAddress, "!badness")
+	err := as.Serve(context.Background())
+	assert.Regexp(t, "FF00151", err)
+
+	// Check we still closed the started channel
+	<-as.Started()
+
+}
+
+func TestWaitForServerStop(t *testing.T) {
+	_, as, done := newTestAPIServer(t, false)
+	defer done()
+
+	open1 := make(chan error)
+	closed1 := make(chan error)
+	close(closed1)
+	as.waitForServerStop(open1, closed1)
+	as.waitForServerStop(closed1, open1)
+
+}
+
+func TestBadRoute(t *testing.T) {
+	apiConfig, metricsConfig, corsConfig := initUTConfig()
+	as := NewAPIServer(context.Background(), APIServerOptions[*utManager]{
+		MetricsRegistry: metric.NewPrometheusMetricsRegistry("ut"),
+		Routes: []*Route{{
+			Extensions: &APIServerRouteExt[string]{}, // T does not match *utManager
+		}},
+		APIConfig:     apiConfig,
+		MetricsConfig: metricsConfig,
+		CORSConfig:    corsConfig,
+	})
+	assert.Panics(t, func() { as.Serve(context.Background()) })
+}
+
+func TestBadSwagger(t *testing.T) {
+	_, as, done := newTestAPIServer(t, false)
+	defer done()
+
+	h := as.swaggerHandler(func(req *http.Request) (*openapi3.T, error) {
+		return nil, fmt.Errorf("pop")
+	})
+	res := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "swagger", nil)
+	assert.NoError(t, err)
+	status, err := h(res, req)
+	assert.Equal(t, 500, status)
+	assert.Regexp(t, "pop", err)
+}
+
+func TestBadMetrics(t *testing.T) {
+	_, as, done := newTestAPIServer(t, false)
+	defer done()
+	as.MetricsRegistry = metric.NewPrometheusMetricsRegistry("wrong")
+	assert.Panics(t, func() { as.createMetricsMuxRouter(context.Background()) })
+}
