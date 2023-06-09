fe6e22d6c34ee5427e8abf88b44625a5a35801c7
Enrique Lacal <enrique.lacal@kaleido.io>
2023-06-09 11:12:29 +0100

    revert: add tests for wsclient e2e
    Signed-off-by: Enrique Lacal <enrique.lacal@kaleido.io>

--- pkg/wsclient/wsclient_test.go
+++ pkg/wsclient/wsclient_test.go
@@ -48,6 +48,15 @@ func TestWSClientE2ETLS(t *testing.T) {
 	defer close()
 	assert.NoError(t, err)
 
+	first := true
+	beforeConnect := func(ctx context.Context) error {
+		if first {
+			first = false
+			return fmt.Errorf("first run fails")
+		}
+		return nil
+	}
+
 	afterConnect := func(ctx context.Context, w WSClient) error {
 		return w.Send(ctx, []byte(`after connect message`))
 	}
@@ -72,13 +81,21 @@ func TestWSClientE2ETLS(t *testing.T) {
 		RootCAs:      rootCAs,
 	}
 
-	wsc, err := New(context.Background(), wsConfig, nil, afterConnect)
+	wsc, err := New(context.Background(), wsConfig, beforeConnect, afterConnect)
 	assert.NoError(t, err)
 
 	//  Change the settings and connect
 	wsc.SetURL(wsc.URL() + "/updated")
 	err = wsc.Connect()
 	assert.NoError(t, err)
+
+	// Test server rejects a 2nd connection
+	wsc2, err := New(context.Background(), wsConfig, beforeConnect, afterConnect)
+	assert.NoError(t, err)
+	wsc2.SetURL(wsc2.URL() + "/updated")
+	err = wsc2.Connect()
+	assert.Error(t, err)
+	wsc2.Close()
 
 	// Receive the message automatically sent in afterConnect
 	message1 := <-toServer
