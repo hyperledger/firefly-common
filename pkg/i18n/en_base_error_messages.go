fd2e3f449ae47c8d1ea9d3b20f8c552cb8bb0f61
Peter Broadhurst <peter.broadhurst@kaleido.io>
2024-02-12 11:49:03 -0500

    Add direct exec option
    Signed-off-by: Peter Broadhurst <peter.broadhurst@kaleido.io>

--- pkg/i18n/en_base_error_messages.go
+++ pkg/i18n/en_base_error_messages.go
@@ -180,4 +180,5 @@ var (
 	MsgJSONQuerySortUnsupported                    = ffe("FF00242", "Invalid 'order' for sort (must be 'asc', 'ascending', 'desc' or 'descending'): %s", 400)
 	MsgWebSocketBatchInflight                      = ffe("FF00243", "Stream '%s' already has batch '%d' inflight on websocket connection '%s'")
 	MsgWebSocketRoundTripTimeout                   = ffe("FF00244", "Timed out or cancelled waiting for acknowledgement")
+	MsgDBExecFailed                                = ffe("FF00245", "Database update failed")
 )
